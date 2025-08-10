ec0b4de66878fe10af426045861ec982ef68a970
Harry Foster <harrywfoster10@gmail.com>
2025-08-10 19:54:44 +0100

    Added a basic imgui
    Joke of the commit:
    What's the difference between a hot potato and a flying pig?
    
    One's a heated yam, the other's a yeeted ham.

---
+++ playground/imgui/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/link.d"
+  "CMakeFiles/main.dir/include/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/main.dir/include/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/main.dir/include/imgui/backends/imgui_impl_opengl2.cpp.o"
+  "CMakeFiles/main.dir/include/imgui/backends/imgui_impl_opengl2.cpp.o.d"
+  "CMakeFiles/main.dir/include/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/main.dir/include/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/main.dir/include/imgui/imgui.cpp.o"
+  "CMakeFiles/main.dir/include/imgui/imgui.cpp.o.d"
+  "CMakeFiles/main.dir/include/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/main.dir/include/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/main.dir/include/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/main.dir/include/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/main.dir/include/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/main.dir/include/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/main.dir/include/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/main.dir/include/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/core/Camera.cpp.o"
+  "CMakeFiles/main.dir/src/core/Camera.cpp.o.d"
+  "CMakeFiles/main.dir/src/core/Shader.cpp.o"
+  "CMakeFiles/main.dir/src/core/Shader.cpp.o.d"
+  "CMakeFiles/main.dir/src/core/Texture.cpp.o"
+  "CMakeFiles/main.dir/src/core/Texture.cpp.o.d"
+  "CMakeFiles/main.dir/src/glad/glad.c.o"
+  "CMakeFiles/main.dir/src/glad/glad.c.o.d"
+  "CMakeFiles/main.dir/src/stb/stb_image.cpp.o"
+  "CMakeFiles/main.dir/src/stb/stb_image.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ playground/imgui/lightfragment.frag
@@ -0,0 +1,12 @@
+#version 330 core
+
+uniform vec2 mouse_pos;    // in pixels, Y already flipped
+uniform vec2 window_res;   // in pixels
+
+out vec4 FragColor;
+
+void main()
+{
+
+    FragColor = vec4(1.0);
+}
